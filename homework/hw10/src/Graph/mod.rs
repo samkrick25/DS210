e3a819ef6e5df23e7706bfc20a354e0f3cdac7d1
Sam Krick <samkrick@bu.edu>
2024-04-15 18:46:21 -0400

    finish print, fix comments for hw10

--- homework/hw10/src/Graph/mod.rs
+++ homework/hw10/src/Graph/mod.rs
@@ -2,18 +2,18 @@ use rand::Rng;
 use std::fs::File;
 use std::io::prelude::*;
 
-mod graph {
+mod Graph {
     pub type Vertex = usize;
     pub type AdjacencyList = Vec<Vec<Vertex>>;
     pub type EdgeList = Vec<(usize, usize)>;
     //create graph struct for storing number of vertices, edge list, and adjacency list
-    pub struct Graph {
+    pub struct GraphStruct {
         pub n: usize,
         pub edges: EdgeList,
         pub adjacencylist: AdjacencyList,
     }
 
-    impl Graph {
+    impl GraphStruct {
         pub fn edge_list_from_file(path: &str) -> (EdgeList, usize) {
             //this will read the file given by professor, and return an edge list and a usize that represents the number of vertices
             //in the graph, which corresponds to the first line of the file
@@ -51,13 +51,14 @@ mod graph {
             adjacencylist
         }
 
-        pub fn create_graph(n: &usize, edges: &EdgeList, adjacencylist: AdjacencyList) -> Graph {
+        pub fn create_graph(n: &usize, edges: &EdgeList, adjacencylist: AdjacencyList) -> GraphStruct {
             //just take the number of vertices, edge list, and adjacency list created from previous functions and return a Graph object
             Graph {n, edges, adjacencylist}
         }
     }
     pub mod PageRank {
-        pub fn random_walk(graph: &Graph) -> Vec<usize> {
+        //ADD COMMENTS AND DESCRIBE CODE
+        pub fn random_walk(graph: &GraphStruct) -> Vec<usize> {
             let mut end_of_walks = vec![0; graph.n];
             rng = rand::thread_rng();
             for _ in 0..99 {
@@ -69,15 +70,19 @@ mod graph {
                     else {
                         selection = rng.gen_range(0..9);
                         if selection = 0 {
-                            current_node = choose_new_node(current_node)
+                            current_node = choose_new_node(current_node);
                         }
                         if 0 < selection <= 9 {
-
+                            current_neighbors = graph.adjacencylist[current_node];
+                            current_node = current_neighbors.choose();
                         }
                     }
                 }
-            } 
+                end_of_walks[current_node] += 1;
+            }
+            end_of_walks //TURN INTO A PRIORITY QUEUE? I need some way to save vertex identity
         }
+
         pub fn choose_new_node(current_node: Vertex) -> Vertex {
             //this function is written to pick a random node to start from. it takes the current_node as an input returns a new node
             //A while loop is used to ensure that the picked node is different than the current node. I create an rng thread, then
@@ -88,5 +93,13 @@ mod graph {
                 new_node = rng.gen_range(0..999);
             }
             new_node
+        }
+
+        pub fn print_top_5(end_of_walks: Vec<usize>, graph: GraphStruct) {
+            //pop off priority queue 5 times, put those into top_5_nodes, then do rest of stuff and print
+            let top_5_nodes = end_of_walks[..5];
+            let top_5_pagerank = top_5_nodes as f64 / 100*graph.n as f64;
+            println!("")
+        }
     }
 }

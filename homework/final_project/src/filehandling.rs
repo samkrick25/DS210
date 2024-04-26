ec4e807ab430e94571ad884bd5fcc61805b17b0d
Sam Krick <160279286+samkrick25@users.noreply.github.com>
2024-04-26 17:05:02 -0400

    filehandling functions written for final project

--- homework/final_project/src/filehandling.rs
+++ homework/final_project/src/filehandling.rs
@@ -1,9 +1,13 @@
 use std::io::prelude::*;
 use std::fs::File;
 use urlencoding::decode;
+use std::collections::HashMap;
 
-fn read_articles(path: &str) -> Vec<String> {
-    let mut articles = vec![];
+type ArticleMap = HashMap<String, usize>;
+type EdgeList = Vec<(usize, usize)>;
+
+fn read_articles(path: &str) -> ArticleMap {
+    let mut articles = HashMap::<String, usize>::new();
     let file = File::open(path).expect("File failed to open!");
     let bufreader = std::io::BufReader::new(file);
     let lines = bufreader.lines().enumerate();
@@ -13,26 +17,36 @@ fn read_articles(path: &str) -> Vec<String> {
             continue
         }
         let decodedarticle = decode(&articleurl).expect("Failed to decode article name!");
-        articles.push(decodedarticle);
+        articles.insert(String::from(decodedarticle), line_number);
     }
     articles
 }
 
 fn read_edges(path: &str) -> Vec<(String, String)> {
-    let mut edges = vec![];
+    let mut str_edges = vec![];
     let file = File::open(path).expect("File failed to open!");
     let bufreader = std::io::BufReader::new(file):
     let lines = bufreader.lines().enumerate();
     for (line_number, line_result) in lines {
-        let linkurl = line_result.unwrap();
+        let edge_url = line_result.unwrap();
         if line_number < 13 {
             continue
         }
-        let decodedlink = decode(&linkurl).expect("Failed to decode link!");
-        edgeiter = decodedlink.split("\t");
-        if let (Some(article1), Some(article2)) = (edgeiter.next(), edgeiter.next()){
-            edges.push((article1.to_string(), article2.to_string()));
+        let decoded_link = decode(&edge_url).expect("Failed to decode link!");
+        edge_iter = decoded_link.split("\t");
+        if let (Some(article1), Some(article2)) = (edge_iter.next(), edge_iter.next()){
+            str_edges.push((article1.to_string(), article2.to_string()));
         }
     }
     edges
+}
+
+fn edge_string_to_number(articles: ArticleMap, edges: Vec<(String, String)>) -> EdgeList {
+    let mut edges_num = vec![];
+    for (article1, article2) in edges {
+        let article1_num = articles[article1];
+        let article2_num = articles[article2];
+        edges_num.push((article1_num, article2_num));
+    }
+    edges_num
 }
